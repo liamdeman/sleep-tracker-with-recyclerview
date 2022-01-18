44f7d6bf59c3b509d075175e41d7254355582354
Liam Deman <liam.deman@student.hogent.be>
2022-01-19 00:11:32 +0100

    5. Display SleepQuality Data

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -61,6 +61,13 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {x ->
+            adapter.data = x
+        })
+
         binding.lifecycleOwner = this
 
         // Add an Observer on the state variable for showing a Snackbar message
